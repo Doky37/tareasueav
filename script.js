fd31cc65ca4fb1486b60b46b9c2d3a4bd8e15f62
Doky37 <124918805+Doky37@users.noreply.github.com>
2025-03-07 23:30:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let p1 = "Saber las nociones básicas del lenguaje HTML te ayuda a desenvolverte en el entorno tecnológico, junto con el CSS y JavaScript son fundamentales para la creación de páginas web y aplicaciones. Con esto en mente puedes usar estas herramientas para todos tus proyectos desarrollados en el entorno digital. En nuestra carrera de arte y comunicación estas tecnologías se aplican para crear contenido interactivo, portafolios de nuestros trabajos y promocionar nuestros proyectos llevándolos al público. Además de desarrollar la creatividad de cada uno a la hora de construir los códigos de programación.";
+let p2 = "Para crear sitios web que sean interactivos y a su vez creativos implica tener la capacidad de pensar de manera lógica y resolver problemas de forma eficiente. Sobre todo cuando se trata de un trabajo colaborativo que requiere de la participación de todos los integrantes, cada uno debe tener algo que aporte a la realización del trabajo y la habilidad de encontrar soluciones en caso de suceder cualquier contratiempo. Así, las tecnologías web permiten el aprendizaje no solamente autónomo sino también colaborativo.";
+
+document.getElementById("parr1").innerHTML=p1;
+document.getElementById("parr2").innerHTML=p2;
+
+console.log(p1.length);
+console.log(p1.length);
+
